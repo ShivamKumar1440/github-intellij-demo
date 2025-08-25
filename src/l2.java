db742ae08399ef13c33d5be37063bd7c5cb2558b
shivam <shivam.2428cseai1440@kiet.edu>
2025-08-25 11:19:57 +0530

    push it

--- src/l2.java
+++ src/l2.java
@@ -2,5 +2,6 @@ public class l2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello and welcome!");
+        System.out.println("ohh yahh daddy please");
     }
 }
